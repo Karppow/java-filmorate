7c9320837fa136fcea8bb9bd4fca8dc34b50a77e
karppow <tesserackt@yandex.ru>
2025-04-01 16:22:30 +0300

    поправил пробел

--- src/main/java/ru/yandex/practicum/filmorate/validator/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/UserValidator.java
@@ -7,13 +7,13 @@ import java.util.Date;
 
 public class UserValidator {
     public void validate(User user) {
-        if (user.getEmail() == null || user.getEmail().isEmpty() ||!user.getEmail().contains("@")) {
+        if (user.getEmail() == null || user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
             throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
         }
         if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
             throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         }
-        if (user.getBirthday()!= null && user.getBirthday().after(new Date())) {
+        if (user.getBirthday() != null && user.getBirthday().after(new Date())) {
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
     }

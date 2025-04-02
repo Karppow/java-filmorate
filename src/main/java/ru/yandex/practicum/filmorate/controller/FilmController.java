b4f45796602b63fadc02146fcdcaab516748a219
karppow <tesserackt@yandex.ru>
2025-04-02 18:09:15 +0300

    Исправлено

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,9 +20,11 @@ import java.util.Map;
 public class FilmController {
     private final Logger logger = LoggerFactory.getLogger(FilmController.class);
     private final Map<Long, Film> films = new HashMap<>();
+    private final LocalDate targetDate = LocalDate.of(1895, 12, 28);
 
     @PostMapping
     public ResponseEntity<Film> createFilm(@Valid @RequestBody Film film) {
+        valid(film);
         logger.info("Creating film: {}", film);
         long id = films.size() + 1;
         film.setId(id);
@@ -30,6 +34,7 @@ public class FilmController {
 
     @PutMapping
     public ResponseEntity<Film> updateFilm(@Valid @RequestBody Film film) {
+        valid(film);
         logger.info("Updating film: {}", film);
         Long id = film.getId(); // Получаем ID из объекта Film
         if (id != null && films.containsKey(id)) {
@@ -46,4 +51,11 @@ public class FilmController {
         logger.info("Getting all films");
         return ResponseEntity.ok(new ArrayList<>(films.values()));
     }
+
+    private void valid(Film film) {
+        if (film.getReleaseDate().isBefore(targetDate)) {
+            throw new ValidationException("Дата выхода позже 1895");
+        }
+    }
+
 }

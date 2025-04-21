85d76fd46c7857b5f706c37c1bfcc7334ff7838d
karppow <tesserackt@yandex.ru>
2025-04-21 20:29:07 +0300

    поправил ошибки

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.Exception.FilmNotFoundException;
-import ru.yandex.practicum.filmorate.Exception.LikeAlreadyExistsException;
 import ru.yandex.practicum.filmorate.Exception.LikeNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
